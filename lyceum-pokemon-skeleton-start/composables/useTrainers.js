7b1f5cf88d6f6757e35b314540265865a1fad872
kade-P <kkadena@gmail.com>
2023-03-17 09:48:46 +0900

    Delete lyceum-pokemon-skeleton-start directory

--- lyceum-pokemon-skeleton-start/composables/useTrainers.js
+++
@@ -1,7 +0,0 @@
-import { useFetch, useRuntimeConfig } from "#app";
-
-export default () => {
-  const config = useRuntimeConfig();
-  const response = useFetch(`${config.backendOrigin}/api/trainers`);
-  return response;
-};
